846e25a4d75d21f4a5a52060299cbe018a42c219
matala155 <matejkubesak@seznam.cz>
2021-10-23 15:13:52 +0200

    commit

--- index.php
+++ index.php
@@ -11,11 +11,24 @@
     <link rel="stylesheet" href="">
     <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
     <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+
 </head>
 
 <body>
     <div id="root"></div>
+    <script type="text/babel">
+        // Definice React funkce
+        function HelloWorld() {
+            return <h1>Hello, world!</h1>;
+    }
 
+        // Vykreslení instance komponenty do HTML struktury
+        ReactDOM.render(
+            <HelloWorld />, // Vytvoření komponenty
+            document.getElementById('root')  // Připojení do kořene aplikace
+        );
+    </script>
     <script src="js.js" async defer></script>
 </body>
 
--- js.js
+++ js.js
@@ -1,13 +1,9 @@
-class HelloWorld extends React.Component {
-  render() {
-    // Vykreslení komponenty
-    // Vykreslení vrátí element <h1> s textem "Hello World"
-    return React.createElement("h1", null, "Hello World!");
-  }
+function HelloWorld() {
+  return <h1>Hello, world!</h1>;
 }
 
 // Vykreslení instance komponenty do HTML struktury
 ReactDOM.render(
-  React.createElement(HelloWorld), // Vytvoření komponenty
+  <HelloWorld />, // Vytvoření komponenty
   document.getElementById("root") // Připojení do kořene aplikace
 );
